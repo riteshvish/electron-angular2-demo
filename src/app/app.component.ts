2425888f372827a1a6f3b197bcfb62cfb00dd504
Ritesh Vishwakarma <Ritesh@cgsadmins-iMac-2.local>
2017-08-04 17:04:02 +0530

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'app-root',
-    templateUrl: 'app.component.html'
+    template: '<router-outlet></router-outlet>'
 })
 
 export class AppComponent { }
